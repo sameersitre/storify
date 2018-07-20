0bdd42f6bc203d59dee688289dd5963889d8cb7c
Sameer <sameersitre@gmail.com>
2018-07-20 15:54:39 +0530

    added new pages

--- storify/src/App.js
+++ storify/src/App.js
@@ -5,6 +5,7 @@ import Button from '@material-ui/core/Button';
 class App extends Component {
   render() {
     return (
+      
       <div className="App">
         hello world
         <Button variant="contained" color="primary">

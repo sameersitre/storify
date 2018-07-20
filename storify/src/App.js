13efab02720bd5ab66a2b90229ca07d9c8f79ef6
harish <harish.djnv@gmail.com>
2018-07-20 19:19:52 +0530

    Login added

--- storify/src/App.js
+++ storify/src/App.js
@@ -1,16 +1,15 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
-import Button from '@material-ui/core/Button';
+import {Button, } from '@material-ui/core/';
+import Login from './_components/Login.js';
+
 class App extends Component {
   render() {
     return (
-      
       <div className="App">
         hello world
-        <Button variant="contained" color="primary">
-        Hello World i am here
-      </Button>
+        <Button variant="contained" color="primary">Hello World i am here</Button>
+        <Login />
       </div>
     );
   }

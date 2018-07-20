312fcc8ff8489b35f4fc73ae8d49f06811352af2
Sameer <sameersitre@gmail.com>
2018-07-20 19:27:14 +0530

    added routes

--- storify/src/App.js
+++ storify/src/App.js
@@ -1,16 +1,39 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {
+  Route,
+  NavLink,
+  HashRouter
+} from "react-router-dom";
+import Home from './_components/Home.js';
+import NavBar from './NavBar.js';
+import Login from './_components/Login.js';
+
 import Button from '@material-ui/core/Button';
 class App extends Component {
   render() {
     return (
-      
+
       <div className="App">
-        hello world
-        <Button variant="contained" color="primary">
-        Hello World i am here
-      </Button>
+        <HashRouter>
+          <div >
+            <ul>
+              <li><NavLink to="/">Cart</NavLink></li>
+              <li><NavLink to="/Home">Home</NavLink></li>
+              <li><NavLink to="/Login">Login</NavLink></li>
+
+            </ul>
+            <div >
+              <Route path="/" component={NavBar} />
+
+              <Route path="/Home" component={Home} />
+              <Route path="/Login" component={Login} />
+
+            </div>
+          </div>
+        </HashRouter>
+        <Button variant="contained" color="primary"></Button>
       </div>
     );
   }

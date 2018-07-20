4349c970c009825ad0fdb2589bc5212692fe51a7
harish <harish.djnv@gmail.com>
2018-07-20 19:20:16 +0530

    Login added

---
+++ storify/src/_components/Login.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import {Button,Input } from '@material-ui/core/';
+
+class Login extends Component {
+    render() {
+        return (
+            <div>
+               <p> Login Into Storify and be mesmarised</p>
+               <div >
+               <Input placeholder="Username"/>
+               <Input placeholder="Password"/>
+               <Button color="green" variant="contained">Submit</Button>
+               </div>
+            </div>
+        )
+    }
+}
+export default Login;
